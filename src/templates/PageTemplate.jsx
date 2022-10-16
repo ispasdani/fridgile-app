a8ccb7fb640726d05e162055d54e04e87389bc6b
ispasdani <ispasdani14@gmail.com>
2022-10-16 23:10:50 +0200

    Added a few components for the purpose of example!

---
+++ src/templates/PageTemplate.jsx
@@ -0,0 +1,15 @@
+import { NavbarMobile } from "../components/navbarMobile/NavbarMobile";
+import { NavbarDesktop } from "../components/navbarDesktop/Navbar";
+import "./pageTemplate.css";
+
+const PageTemplate = (props) => {
+  return (
+    <div className="PageTemplate">
+      <NavbarDesktop />
+      <NavbarMobile />
+      <div className="Content">{props.children}</div>
+    </div>
+  );
+};
+
+export default PageTemplate;
