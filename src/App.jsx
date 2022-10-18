7e42ee4464b30bc8e05df29775c4f0a876c46eff
ispasdani <ispasdani14@gmail.com>
2022-10-18 10:53:01 +0200

    added react router and path to object Detector

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import PageTemplate from "./templates/PageTemplate";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ObjectDetector } from "./components/objectDetector/ObjectDetector";
 
 function App() {
   return (
     <div className="App">
-      <PageTemplate></PageTemplate>
+      <Router>
+        <PageTemplate>
+          <Switch>
+            <Route path="/add">
+              <ObjectDetector />
+            </Route>
+          </Switch>
+        </PageTemplate>
+      </Router>
     </div>
   );
 }
